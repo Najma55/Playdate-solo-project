3886bc95abdbab1f7b660d3acf867f3e037935cc
Najma Jama <Najma55.nj@gmail.com>
2023-10-13 22:13:11 -0500

    update invite page component and style it in css

--- src/components/InvitePage/InvitePage.jsx
+++ src/components/InvitePage/InvitePage.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import "./InvitePage.css";
+import Nav from "../Nav/Nav";
 
-export default function InvitePage(){
-    <div></div>
+export default function InvitePage() {
+  return (
+    <div className="content-wrapper">
+        <Nav/>
+      <div className="invitation-details">
+        <h4>
+          You have been invited by parent x to attend event y with them on 01/05/24
+        </h4>
+      </div>
+      <button className="joinbutton">Join Them</button>
+      <h5 className="event-address">2425 50th St, Minneapolis, Minnesota</h5>
+    </div>
+  );
 }
