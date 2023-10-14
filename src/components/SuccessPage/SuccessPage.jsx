5c67c964340fa8cf23c3c2e77c9979ef6bd4bc5a
Najma Jama <Najma55.nj@gmail.com>
2023-10-14 14:58:18 -0500

    add nav also style css

--- src/components/SuccessPage/SuccessPage.jsx
+++ src/components/SuccessPage/SuccessPage.jsx
@@ -1,17 +1,21 @@
 import React from "react";
-import "./SuccessPage.css"
+import "./SuccessPage.css";
+import Nav from "../Nav/Nav";
 
 export default function SuccessPage() {
   return (
-    <div className="create-event-form success-page">
-      <div className="success-page-title">
-        <h1>Booking Success!</h1>
-        <p>You have booked to go to event y on 01/03/23</p>
+    <>
+      <Nav />
+      <div className="create-event-form success-page">
+        <div className="success-page-title">
+          <h1>Booking Success!</h1>
+          <p>You have booked to go to event y on 01/03/23</p>
+        </div>
+        <div className="success-page-title">
+          <p>Share your invite link with other parents to invite them!</p>
+          <button className="book copy">Copy Invite Link</button>
+        </div>
       </div>
-      <div className="success-page-title">
-        <p>Share your invite link with other parents to invite them!</p>
-        <button className="book copy">Copy Invite Link</button>
-      </div>
-    </div>
+    </>
   );
 }
